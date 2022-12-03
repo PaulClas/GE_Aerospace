c3f25eb8488bb5115e983a30709d2eb2f66e0d5f
Paul Clas <clas.paul@gmail.com>
2022-12-03 13:17:41 -0500

    PPT project init

---
+++ PPT_PCLAS_GE/PPT_PCLAS_GE/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using PPT_PCLAS_GE.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPT_PCLAS_GE.MVVM.View
+{
+    class MainViewModel: ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
